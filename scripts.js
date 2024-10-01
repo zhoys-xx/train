ef2f94f35ed50a033647da1af9a0588a9335d2bb
zhoys-xx <yh_zhou03@163.com>
2024-10-01 21:21:54 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// 图片滑动效果
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// 初始化显示第一张图片
+showSlide(currentSlide);
